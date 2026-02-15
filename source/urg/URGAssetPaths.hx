f90aa7c8802a38c578cac5947953893eeab52c03
Macohi <sinconsistencia@gmail.com>
2026-02-15 13:28:24 -0600

    doh

--- source/urg/URGAssetPaths.hx
+++ source/urg/URGAssetPaths.hx
@@ -5,5 +5,5 @@ import macohi.funkin.koya.backend.AssetPaths;
 class URGAssetPaths
 {
 	public static function chart(song:String, ?library:String)
-		return AssetPaths.json('data/$song', library);
+		return AssetPaths.json('data/songs/$song', library);
 }
