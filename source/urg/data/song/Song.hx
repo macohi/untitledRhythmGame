d846173ceac009919e2bb0c511d60992337b8e65
Macohi <sinconsistencia@gmail.com>
2026-02-15 12:11:25 -0600

    load chart paths

--- source/urg/data/song/Song.hx
+++ source/urg/data/song/Song.hx
@@ -1,5 +1,10 @@
 package urg.data.song;
 
+import macohi.util.WindowUtil;
+import haxe.Json;
+import macohi.funkin.koya.backend.AssetPaths;
+import macohi.funkin.koya.backend.KoyaAssets;
+
 class Song
 {
 	public static function loadSong(song:String):SongData
@@ -12,8 +17,24 @@ class Song
 			notes: [],
 		};
 
-		if (song == 'test')
-			songData.timeformat = BEATS_AND_STEPS;
+		var chartPath:String = AssetPaths.chart(song, song);
+		if (KoyaAssets.exists(chartPath))
+		{
+			var jsonData:SongData = null;
+
+			try
+			{
+				jsonData = Json.parse(KoyaAssets.getText(chartPath));
+			}
+			catch (e)
+			{
+				WindowUtil.alert('Error loading song chart', 'Chart: ${chartPath}\n\n${e.message}');
+				jsonData = null;
+			}
+
+			if (jsonData != null)
+				songData = jsonData;
+		}
 
 		return songData;
 	}
